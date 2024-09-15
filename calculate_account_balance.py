8222deec8ac8730befda66add3bd6d60df3bdede
Magooch <crvallecito@gmail.com>
2024-09-15 13:17:11 -0600

    Add dictionaries containing customer balances and customer transactions

---
+++ calculate_account_balance.py
@@ -0,0 +1,15 @@
+balances = [{1: 10000}, {2: 7500}, {3: 15000}]
+
+transactions = [
+    {"customer_id": 1, "type": "deposit", "amount": 500},
+    {"customer_id": 2, "type": "deposit", "amount": 1000},
+    {"customer_id": 3, "type": "withdrawal", "amount": 200},
+    {"customer_id": 1, "type": "withdrawal", "amount": 100},
+    {"customer_id": 2, "type": "withdrawal", "amount": 150},
+    {"customer_id": 3, "type": "deposit", "amount": 300},
+    {"customer_id": 1, "type": "deposit", "amount": 250},
+    {"customer_id": 2, "type": "deposit", "amount": 400},
+    {"customer_id": 3, "type": "withdrawal", "amount": 50},
+    {"customer_id": 1, "type": "withdrawal", "amount": 200}
+]
+
