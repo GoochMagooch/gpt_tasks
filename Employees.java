1d003e37b1dae307e0407d96ddb14ec6741694a8
Magooch <crvallecito@gmail.com>
2024-09-15 22:10:19 -0600

    Create source file, constructor for employees and method to create list

---
+++ Employees.java
@@ -0,0 +1,22 @@
+public class EmployeeRecorder {
+    String name;
+    String dept;
+
+    public EmployeeRecorder(String name, String dept) {
+	this.name = name;
+	this.dept = dept;
+    }
+
+    // method that displays employees with their departments in a list
+    public String[][] employeeList() {
+	String[this.name, this.dept] listOfEmployees = new listOfEmployees();
+	System.out.println(listOfEmployees);
+    }
+
+    public static void main(String[] args) {
+
+	
+
+    }
+
+}
