55c9131e8eac30565d52d10c26a5f4aad12ed939
Magooch <crvallecito@gmail.com>
2024-09-15 22:30:39 -0600

    Syntax correction

--- Customer.java
+++ Customer.java
@@ -7,11 +7,11 @@ public class Customer {
 	this.email = email;
     }
 
-    public static void format(String name, String email) {
+    public void format(String name, String email) {
 	System.out.println("Customer Name: " + this.name + ", Email: " + this.email);
     }
     
-    public static void main(String[], args) {
+    public static void main(String[] args) {
 	
 	Customer rochelle = new Customer("Rochelle Williams", "rochelle@gmail.com");
 	Customer sally = new Customer("Sally Mitchell", "sally@gmail.com");
